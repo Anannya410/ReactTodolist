4dc960befe0c075f950c7a38d2bc7a69e2568ebe
Annanya410 <anannya04hiteshi@gmail.com>
2024-04-06 13:37:45 +0530

    Todolist setup stage

---
+++ src/MyComponents/TodoItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function TodoItem() {
+  return (
+    <div>
+      todo works
+    </div>
+  )
+}
+export default TodoItem;
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Todos() {
+  return (
+    <div>
+      todos
+    </div>
+  )
+}
+export default Todos;
